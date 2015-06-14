cc0b23da1dbd95aa9ba43d5dfd7daafc4df89337
efajk <efajk@hotmail.com>
2015-06-14 15:18:41 +0800

    510 add comment

--- 20_TFS.py
+++ 20_TFS.py
@@ -507,6 +507,7 @@ class GeneralAgent(BaseAgent):
 
         # mode write back
         g_intorState[self.index] = self.mode
+        #update defendFoodList
         defendFoodList = self.getFoodYouAreDefending(gameState).asList()
         return moveAction
 
